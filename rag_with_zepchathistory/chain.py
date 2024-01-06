38a2f2ef614ec1fcf2912177edf8c4b61da83388
tobiee <tochukwunwoke1@gmail.com>
2024-01-06 15:27:50 +0100

    implemented chathistorymemory

--- rag_with_zepchathistory/chain.py
+++ rag_with_zepchathistory/chain.py
@@ -1,4 +1,53 @@
+
 import os
+from uuid import uuid4
+from operator import itemgetter
+from typing import Optional, List, Tuple
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+from dotenv import load_dotenv
+
+from langchain.chat_models import ChatOpenAI
+from langchain.memory.chat_message_histories import ZepChatMessageHistory
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.prompts.prompt import PromptTemplate
+from langchain.schema import AIMessage
+from langchain.schema.chat_history import BaseChatMessageHistory
+from langchain.schema.runnable.history import RunnableWithMessageHistory
+from langchain.vectorstores.zep import CollectionConfig, ZepVectorStore
+
+from langchain_core.runnables import (
+    ConfigurableField,
+    RunnableBranch,
+    RunnableLambda,
+    RunnableParallel,
+    RunnablePassthrough,
+)
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.utils import ConfigurableFieldSingleOption
+
+load_dotenv()
+
+ZEP_API_URL = os.environ.get("ZEP_API_URL", "http://localhost:8080")
+
+# RAG answer synthesis prompt
+template = """Answer the question to the best of your knowledge:
+"""
+QA_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        ("system", template),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{question}"),
+    ]
+)
+chain =  QA_PROMPT | ChatOpenAI() | StrOutputParser()
 
 
-# chain = _inputs | ANSWER_PROMPT | ChatOpenAI() | StrOutputParser()
+chain_with_history = RunnableWithMessageHistory(
+    chain,
+    lambda session_id: ZepChatMessageHistory(session_id, url=ZEP_API_URL),
+    history_messages_key="chat_history",
+    input_messages_key="question"
+)
