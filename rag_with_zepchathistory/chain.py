8800b00339c4d34aa3d2a672705a38679837e258
tobiee <tochukwunwoke1@gmail.com>
2024-01-08 21:04:48 +0100

    updated rag_with_zepchathistory/chain.py

--- rag_with_zepchathistory/chain.py
+++ rag_with_zepchathistory/chain.py
@@ -1,28 +1,11 @@
 
 import os
-from uuid import uuid4
-from operator import itemgetter
-from typing import Optional, List, Tuple
-
 
 from langchain.chat_models import ChatOpenAI
 from langchain.memory.chat_message_histories import ZepChatMessageHistory
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.prompts.prompt import PromptTemplate
-from langchain.schema import AIMessage
-from langchain.schema.chat_history import BaseChatMessageHistory
 from langchain.schema.runnable.history import RunnableWithMessageHistory
-from langchain.vectorstores.zep import CollectionConfig, ZepVectorStore
-
-from langchain_core.runnables import (
-    ConfigurableField,
-    RunnableBranch,
-    RunnableLambda,
-    RunnableParallel,
-    RunnablePassthrough,
-)
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables.utils import ConfigurableFieldSingleOption
 
 ZEP_API_URL = os.environ.get("ZEP_API_URL", "http://localhost:8080")
 
@@ -42,6 +25,6 @@ chain =  QA_PROMPT | ChatOpenAI() | StrOutputParser()
 chain_with_history = RunnableWithMessageHistory(
     chain,
     lambda session_id: ZepChatMessageHistory(session_id, url=ZEP_API_URL),
-    # history_messages_key="chat_history",
-    input_messages_key="question"
+    input_messages_key="question",
+    history_messages_key="chat_history",
 )
